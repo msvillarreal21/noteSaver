b01a335893d6969bdf63a12bdc6793447da78fa3
mariasalomevillarreal <msvillarreal2021@gmail.com>
2021-07-17 21:16:09 -0500

    attached the notes.html file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/assets/index.html'));
+    res.sendFile(path.join(__dirname, '../public/assets/html/index.html'));
 })
 
 router.get('/notes', (req, res ) => {
-    res.sendFile(path.join(__dirname, '../public/assets/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/assets/html/notes.html'));
 })
 
 
